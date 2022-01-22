af33db6067b3aebe3e35c9c99fda6f9d84e3b5bf
JHartman0621 <95665285+JHartman0621@users.noreply.github.com>
2022-01-21 19:40:17 -0800

    Add files via upload
    Adding updated PyPoll file.

---
+++ PyPoll.py
@@ -0,0 +1,10 @@
+import csv
+import os
+file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_save = os.path.join("Resources", "election.txt")
+
+with open (file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    headers = next(file_reader) 
+    print(headers)
